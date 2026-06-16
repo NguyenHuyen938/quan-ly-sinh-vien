67ba42e123c1e1a63d1fc028cb61dfca86bcfc65
Tran Duc Minh <tdminh2110@gmail.com>
2026-06-16 19:19:19 +0700

    Comment

--- hello_world.cpp
+++ hello_world.cpp
@@ -6,6 +6,10 @@ int main() {
 	
 	cout << "Hello World" << endl;
 	cout << "Sua 1 ti" << endl;	
+	cout << "Day la file thu nghiem" << endl;
+	cout << "Day la file thu nghiem" << endl;
+	cout << "Day la file thu nghiem" << endl;
+	cout << "Day la file thu nghiem" << endl;
 	cout << "Day la file thu nghiem" << endl;	
 
 	return 0;
